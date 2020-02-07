871a6d82ae628b00d969796b3de778c29422b3f5
Vincent Talbot <talbotv@amotus.ca>
2020-02-06 23:48:23 -0500

    cleanup

--- src/StorageAdapter.php
+++ src/StorageAdapter.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace Talboterie\FlysystemGCPStorage;
 
-use League\Flysystem\Config;
+use Google\Cloud\Core\Exception\NotFoundException;
 use Google\Cloud\Storage\Bucket;
 use Google\Cloud\Storage\StorageObject;
 use League\Flysystem\Adapter\AbstractAdapter;
-use Google\Cloud\Core\Exception\NotFoundException;
+use League\Flysystem\Config;
+use LogicException;
 
 class StorageAdapter extends AbstractAdapter
 {
@@ -21,51 +22,14 @@ class StorageAdapter extends AbstractAdapter
         $this->setPathPrefix($prefix);
     }
 
-    protected function upload(string $path, $content, Config $config): StorageObject
-    {
-        return $this->bucket->upload($content, ['name' => $this->applyPathPrefix($path)]);
-    }
-
-    protected function move(string $path, string $newPath, bool $delete = true): StorageObject
-    {
-        $object = $this->bucket->object($this->applyPathPrefix($path));
-        $newObject = $object->copy($this->bucket->name(), ['name' => $this->applyPathPrefix($newPath)]);
-
-        if ($delete) {
-            $object->delete();
-        }
-
-        return $newObject;
-    }
-
-    public function write($path, $contents, Config $config): StorageObject
-    {
-        return $this->upload($path, $contents, $config);
-    }
-
-    public function writeStream($path, $resource, Config $config): StorageObject
-    {
-        return $this->upload($path, $resource, $config);
-    }
-
-    public function update($path, $contents, Config $config): StorageObject
-    {
-        return $this->upload($path, $contents, $config);
-    }
-
-    public function updateStream($path, $resource, Config $config): StorageObject
-    {
-        return $this->upload($path, $resource, $config);
-    }
-
-    public function rename($path, $newPath): StorageObject
-    {
-        return $this->move($path, $newPath);
-    }
-
     public function copy($path, $newPath): StorageObject
     {
         return $this->move($path, $newPath, false);
+    }
+
+    public function createDir($dirname, Config $config)
+    {
+        throw new LogicException(get_class($this) . ' create directory as needed when writing file. Path: ' . $dirname);
     }
 
     public function delete($path): bool
@@ -91,15 +55,35 @@ class StorageAdapter extends AbstractAdapter
         return true;
     }
 
-    public function createDir($dirname, Config $config)
+    public function getMetadata($path)
     {
-        throw new \LogicException(get_class($this) . ' create directory as needed when writing file. Path: ' . $dirname);
+        return $this->bucket->object($this->applyPathPrefix($path))->info();
     }
 
-    public function setVisibility($path, $visibility)
+    public function getMimetype($path)
     {
-        $object = $this->bucket->object($this->applyPathPrefix($path));
-        $object->update(['acl' => []], ['predefinedAcl' => $visibility]);
+        $meta = $this->getMetadata($path);
+
+        return ['mimetype' => $meta['contentType']];
+    }
+
+    public function getSize($path)
+    {
+        $meta = $this->getMetadata($path);
+
+        return ['size' => $meta['size']];
+    }
+
+    public function getTimestamp($path)
+    {
+        $meta = $this->getMetadata($path);
+
+        return ['timestamp' => strtotime($meta['updated'])];
+    }
+
+    public function getVisibility($path)
+    {
+        $visibility = $this->bucket->object($this->applyPathPrefix($path))->acl()->get();
 
         return compact('path', 'visibility');
     }
@@ -107,20 +91,6 @@ class StorageAdapter extends AbstractAdapter
     public function has($path)
     {
         return $this->bucket->object($this->applyPathPrefix($path))->exists();
-    }
-
-    public function read($path)
-    {
-        $contents = $this->bucket->object($this->applyPathPrefix($path))->downloadAsString();
-
-        return compact('contents');
-    }
-
-    public function readStream($path)
-    {
-        $stream = $this->bucket->object($this->applyPathPrefix($path))->downloadAsStream();
-
-        return compact('stream');
     }
 
     public function listContents($directory = '', $recursive = false)
@@ -141,36 +111,67 @@ class StorageAdapter extends AbstractAdapter
         return $items;
     }
 
-    public function getMetadata($path)
+    public function read($path)
     {
-        return $this->bucket->object($this->applyPathPrefix($path))->info();
+        $contents = $this->bucket->object($this->applyPathPrefix($path))->downloadAsString();
+
+        return compact('contents');
     }
 
-    public function getSize($path)
+    public function readStream($path)
     {
-        $meta = $this->getMetadata($path);
+        $stream = $this->bucket->object($this->applyPathPrefix($path))->downloadAsStream();
 
-        return ['size' => $meta['size']];
+        return compact('stream');
     }
 
-    public function getMimetype($path)
+    public function rename($path, $newPath): StorageObject
     {
-        $meta = $this->getMetadata($path);
-
-        return ['mimetype' => $meta['contentType']];
+        return $this->move($path, $newPath);
     }
 
-    public function getTimestamp($path)
+    public function setVisibility($path, $visibility)
     {
-        $meta = $this->getMetadata($path);
-
-        return ['timestamp' => strtotime($meta['updated'])];
-    }
-
-    public function getVisibility($path)
-    {
-        $visibility = $this->bucket->object($this->applyPathPrefix($path))->acl()->get();
+        $object = $this->bucket->object($this->applyPathPrefix($path));
+        $object->update(['acl' => []], ['predefinedAcl' => $visibility]);
 
         return compact('path', 'visibility');
+    }
+
+    public function update($path, $contents, Config $config): StorageObject
+    {
+        return $this->upload($path, $contents, $config);
+    }
+
+    public function updateStream($path, $resource, Config $config): StorageObject
+    {
+        return $this->upload($path, $resource, $config);
+    }
+
+    public function write($path, $contents, Config $config): StorageObject
+    {
+        return $this->upload($path, $contents, $config);
+    }
+
+    public function writeStream($path, $resource, Config $config): StorageObject
+    {
+        return $this->upload($path, $resource, $config);
+    }
+
+    protected function move(string $path, string $newPath, bool $delete = true): StorageObject
+    {
+        $object = $this->bucket->object($this->applyPathPrefix($path));
+        $newObject = $object->copy($this->bucket->name(), ['name' => $this->applyPathPrefix($newPath)]);
+
+        if ($delete) {
+            $object->delete();
+        }
+
+        return $newObject;
+    }
+
+    protected function upload(string $path, $content, Config $config): StorageObject
+    {
+        return $this->bucket->upload($content, ['name' => $this->applyPathPrefix($path)]);
     }
 }
